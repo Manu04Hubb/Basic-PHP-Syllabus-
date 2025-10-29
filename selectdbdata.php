c557d4449a5b72c51a6a1d3f57d9cafc630b52f5
Manu04Hubb <emanuelodhiambo84@gmail.com>
2025-10-29 10:52:15 +0300

    Removed php from it

--- selectdbdata.php
+++ selectdbdata.php
@@ -1,33 +1,4 @@
-<?php
-//We use select mainly while searching or retrieving data from the database
-// Common use case creating a search system where users can search for specific records
-// based on certain criteria
-//Similar to insert, update and delete we use prepared statements to prevent SQL injection attacks
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    $user_search = $_POST['searchname'];
-   
-
-    try {
-        require_once 'dbconnection.php';
-        $sql_query = "SELECT * FROM users_talbe WHERE username = :usersearch;";
-
-        $stmt = $pdo->prepare($sql_query);
-
-        $stmt->bindParam(':usersearch', $user_search);
-
-        $results->$stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        $stmt->execute();
-        $pdo = null; // Close the database connection
-        $stmt = null; // Close the statement
-        die();//use die whenever there is an SQL connection to prevent further execution of the script
-    }catch (PDOException $e){
-        echo "Error: " . $e->getMessage();
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
